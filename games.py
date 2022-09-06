7b1266fafce3adcf0eff75c36cf13920fd7a9b3e
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-06 14:09:38 +0300

    never mind

---
+++ games.py
@@ -0,0 +1,16 @@
+fixture=[['Veterans', 'Clippers'],
+ ['Pirates', 'Snipers'],
+  ['Zambarani', 'Veterans'], 
+  ['Yanga', 'Sigalagala'],
+   ['Sigalagala', 'Gongagonga'],
+    ['Rovers', 'Yanga'], 
+    ['Nurses', 'Rovers'], 
+    ['Snipers', 'Black Eagles'], 
+    ['Clippers', 'Zambarani'],
+ ['Rangers', 'Nurses']]
+
+for i in range(len(fixture)):
+    for j in range(len(fixture)):
+        pass
+
+
