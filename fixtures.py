c482dfcf18c26e5b6653bb0ac30d0ecb6639e7e1
JohnM-Kahura <Johnmungai392@gmail.com>
2022-09-06 12:54:20 +0300

    algorithims to generate fixtures  for a league

--- fixtures.py
+++ fixtures.py
@@ -1,6 +1,20 @@
 import random
 
-home=[1,2,3,4,5,6,7,8,9,10]
-away=[1,2,3,4,5,6,7,8,9,10]
-random.shuffle(home)
-print(home)
+teams1=["Gongagonga","Zambarani","Black Eagles","Nurses","Clippers","Sigalagala","Veterans","Pirates","Snipers","Rangers","Rovers","Yanga"]
+teams2=["Gongagonga","Zambarani","Black Eagles","Nurses","Clippers","Sigalagala","Veterans","Pirates","Snipers","Rangers","Rovers","Yanga"]
+
+random.shuffle(teams1)
+random.shuffle(teams2)
+# print(teams1)
+# print(teams2)
+fixture=[]
+num=10
+for team1,team2 in zip(teams1,teams2):
+    print(team1 + " vs " +team2)
+    if team1==team2:
+        print("Fixture Not genarated try again")
+        break
+    current_fixture=[team1,team2]
+    fixture.append(current_fixture)
+print(fixture)    
+    
